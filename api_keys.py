8c3dc66f6c78b76d79a154e2f14e6a878ab2fd7a
Aidan David <132293345+AidanDavid@users.noreply.github.com>
2024-10-10 17:44:31 -0400

    complete

--- api_keys.py
+++ api_keys.py
@@ -1,6 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "f720405cfcb4db368288a81ab0581b8f"
+weather_api_key = "YOUR_KEY_HERE"
 
 # Geoapify API Key
-geoapify_key = "f76d79fa87cc4d15938a69254b2ed547"
+geoapify_key = "YOUR_KEY_HERE"
 
